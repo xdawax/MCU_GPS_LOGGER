8fc6c4a2ce96be7ad4e2437f95423b746203c24d
xdawax <david.carlsson.6652@student.uu.se>
2019-02-02 14:27:16 +0100

    Add project file

---
+++ MCU_GPS_LOGGER/MCU_GPS_LOGGER/main.c
@@ -0,0 +1,18 @@
+/*
+ * MCU_GPS_LOGGER.c
+ *
+ * Created: 2019-02-02 14:26:41
+ * Author : David
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
