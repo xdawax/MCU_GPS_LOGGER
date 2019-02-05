1a6637718401f50b63d303206e9d85d8cdb61c92
xdawax <david.carlsson.6652@student.uu.se>
2019-02-05 12:17:41 +0100

    Add RW working

--- MCU_GPS_LOGGER/MCU_GPS_LOGGER/main.c
+++ MCU_GPS_LOGGER/MCU_GPS_LOGGER/main.c
@@ -6,15 +6,45 @@
  */ 
 #include <avr/io.h>
 #include "USART.h"
+#include "EEPROM.h"
+
+void output(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
+	USART_transmit_byte(a);
+	USART_transmit_byte(b);
+	USART_transmit_byte(c);
+	USART_transmit_byte(d);
+	USART_transmit_byte('\n');
+	USART_transmit_byte('\r');
+}
 
 int main(void)
 {
     USART_init();
-
+	
+	uint8_t a = 'a';
+	uint8_t b = 'b';
+	uint8_t c = 'c';
+	uint8_t d = 'd';
+	
+	USART_transmit_string("BEFORE:");
+	output(a, b, c, d);
+	
+	EEPROM_write_byte(a, 0x00);
+	EEPROM_write_byte(b, 0x01);
+	EEPROM_write_byte(c, 0x02);
+	EEPROM_write_byte(d, 0x03);
+	
+	a = EEPROM_read_byte(0x03);
+	b = EEPROM_read_byte(0x02);
+	c = EEPROM_read_byte(0x01);
+	d = EEPROM_read_byte(0x00);
+	
+	USART_transmit_string("AFTER:");
+	output(a, b, c, d);
     /* Replace with your application code */
     while (1) 
-    {
-	USART_receive_byte();
+    {	
+		
     }
 	return 0;
 }
