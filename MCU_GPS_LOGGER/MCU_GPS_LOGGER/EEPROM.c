66a14b13a9e3774af4b344c8525ba460cfa8b859
xdawax <david.carlsson.6652@student.uu.se>
2019-02-05 11:48:43 +0100

    Add write byte function

--- MCU_GPS_LOGGER/MCU_GPS_LOGGER/EEPROM.c
+++ MCU_GPS_LOGGER/MCU_GPS_LOGGER/EEPROM.c
@@ -4,3 +4,26 @@
  * Created: 2019-02-05 11:25:08
  *  Author: dawa
  */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void EEPROM_write_byte(uint8_t byte, uint16_t address) {
+	cli();								// disable interrupts so we don't get interrupted between seting the master write and write
+	while(EECR & (1 << EEPE));			// wait until previous write is completed
+	while(SELFPRGEN & (1 << SPMCSR));
+	
+	EEAR = address;		// write address into address register
+	EEDR = byte;		// write data into data register
+	
+	EECR |= (1 << EEMPE);	// set Master Write Enable
+	EECR |= (1 << EEPE);	// within 4 clock cycles enable write
+	
+	sei();								// enable the interrupts
+}
+
+uint8_t EEPROM_read_byte(uint16_t address) {
+	uint8_t byte = -1;
+	
+	return byte;
+}
--- MCU_GPS_LOGGER/MCU_GPS_LOGGER/EEPROM.h
+++ MCU_GPS_LOGGER/MCU_GPS_LOGGER/EEPROM.h
@@ -9,8 +9,9 @@
 #ifndef EEPROM_H_
 #define EEPROM_H_
 
+void EEPROM_write_byte(uint8_t byte, uint16_t address);
 
-
+uint8_t EEPROM_read_byte(uint16_t address);
 
 
 #endif /* EEPROM_H_ */
