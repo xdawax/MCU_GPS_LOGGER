2a0d50e758cd7f362313a58ce6d442c24ef6d7be
xdawax <david.carlsson.6652@student.uu.se>
2019-02-05 11:58:44 +0100

    Add read byte

--- MCU_GPS_LOGGER/MCU_GPS_LOGGER/EEPROM.c
+++ MCU_GPS_LOGGER/MCU_GPS_LOGGER/EEPROM.c
@@ -7,7 +7,10 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "avr/delay.h"
 
+
+// automatically set to atomically RW
 void EEPROM_write_byte(uint8_t byte, uint16_t address) {
 	cli();								// disable interrupts so we don't get interrupted between seting the master write and write
 	while(EECR & (1 << EEPE));			// wait until previous write is completed
@@ -18,12 +21,22 @@ void EEPROM_write_byte(uint8_t byte, uint16_t address) {
 	
 	EECR |= (1 << EEMPE);	// set Master Write Enable
 	EECR |= (1 << EEPE);	// within 4 clock cycles enable write
-	
+	_delay_ms(5);			// write takes about 3-4 ms, R.I.P run in peace
 	sei();								// enable the interrupts
 }
 
 uint8_t EEPROM_read_byte(uint16_t address) {
+	cli();							// disable interrupts
+	
 	uint8_t byte = -1;
 	
+	while(EECR & (1 << EEPE));	// check that no one is writing data to EEPROM
+	
+	EEAR = address;					// set address to read from
+	EECR |= (1 << EERE);			// enable reading 
+	
+	byte = EEDR;
+	
+	sei();							// enable interrupts
 	return byte;
 }
