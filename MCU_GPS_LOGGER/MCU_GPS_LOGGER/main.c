ef2eb80ef7c1b8d989d800a0675bb3f057ef9ff1
xdawax <david.carlsson.6652@student.uu.se>
2019-02-07 13:42:21 +0100

    Add small test suites

--- MCU_GPS_LOGGER/MCU_GPS_LOGGER/main.c
+++ MCU_GPS_LOGGER/MCU_GPS_LOGGER/main.c
@@ -23,8 +23,10 @@ int main(void)
 	
 	EEPROM_clear();
 	
-	test_RW_word();
-
+	
+	// can only run one test at the time since they will use same address-space
+	//test_RW_word();
+	test_RW_byte();
 	/* Replace with your application code */
     while (1) 
     {	
@@ -43,10 +45,12 @@ void output(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
 }
 
 void test_RW_word() {
+	USART_transmit_string("TESTING: test_RW_word\n\r");
 	uint8_t values[4] = {0,0,0,0};
 	
 	uint32_t word = 0b00000001000000100000001100000100;  // [1,2,3,4] as seperate bytes
 	
+	USART_transmit_string("Expected output 1234: ");
 	EEPROM_write_word_next_free(word);
 	
 	for (int i = FIRST_DATA_BYTE; i < FIRST_DATA_BYTE + 4; i++)
@@ -54,29 +58,34 @@ void test_RW_word() {
 		values[i] = EEPROM_read_byte(i);
 		USART_transmit_byte(values[i] + '0');
 	}
+	USART_transmit_string("\n\r");
+	USART_transmit_string("TEST COMPLETED: test_RW_word\n\n\r");
 	
 }
 void test_RW_byte() {
+	
+	USART_transmit_string("TESTING: test_RW_byte\n\r");
 	uint8_t a = '1';
 	uint8_t b = '2';
 	uint8_t c = '3';
 	uint8_t d = '4';
 	
-	_delay_ms(100);
+	USART_transmit_string("Expected output 1234: ");
 	output(a, b, c, d);
-	/*
+	
 	EEPROM_write_byte_next_free(a);
 	EEPROM_write_byte_next_free(b);
 	EEPROM_write_byte_next_free(c);
 	EEPROM_write_byte_next_free(d);
-	*/
+	
 	a = EEPROM_read_byte(0x05);
 	b = EEPROM_read_byte(0x04);
 	c = EEPROM_read_byte(0x03);
 	d = EEPROM_read_byte(0x02);
 	
+	USART_transmit_string("Expected output 4321: ");
 	output(a, b, c, d);
-    
+	
 	EEPROM_write_byte_next_free('a');
 	EEPROM_write_byte_next_free('b');
 	EEPROM_write_byte_next_free('c');
@@ -87,5 +96,7 @@ void test_RW_byte() {
 	c = EEPROM_read_byte(0x07);
 	d = EEPROM_read_byte(0x06);
 		
+	USART_transmit_string("Expected output dcba: ");
 	output(a, b, c, d);
+	USART_transmit_string("TEST COMPLETED: test_RW_byte\n\n\r");
 }
