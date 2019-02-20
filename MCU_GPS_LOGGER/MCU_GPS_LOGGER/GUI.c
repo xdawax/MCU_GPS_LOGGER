882cb55156c6c2be19a1d3d16cfb315766f7257c
Unknown <magnus.ja.gustafsson@gmail.com>
2019-02-20 12:44:29 +0100

    Add can now goto "display path" in menu

--- MCU_GPS_LOGGER/MCU_GPS_LOGGER/GUI.c
+++ MCU_GPS_LOGGER/MCU_GPS_LOGGER/GUI.c
@@ -42,11 +42,20 @@ ISR(PCINT2_vect)
 	cli();
 	if(bit_is_clear(PIND,PD5))
 	{
-		PORTC |= (1 << PC5);  // set PC5 to 1
 		select_option = (select_option+1)%NUM_OPTION;
 		draw_screen();
-	} else {
-		PORTC &= ~(1 << PC5); // re-set PC5 to 0
+	}
+	if(bit_is_clear(PIND,PD6)) {
+		if (is_showing_path) {
+			is_showing_path = 0;
+		} else if (select_option == MENU_START_STOP_LOGGING) {
+			is_logging = !is_logging;
+		} else if (select_option == MENU_SHOW_PATH) {
+			// --- switch to showing path mode here --- 
+			is_showing_path = 1;
+			PORTC |= (1 << PC5);
+		} 
+		draw_screen();	
 	}
 	sei();
 }
@@ -54,23 +63,41 @@ ISR(PCINT2_vect)
 
 void init_pin_change_interrupt21(void)
 {
-	PCICR |= (1 << PCIE2); //SET PCIE2 for enabling interrupts ON portD
+	PCICR |= (1 << PCIE2); // SET PCIE2 for enabling interrupts ON portD
 	PCMSK2 |= (1 << PCINT21); // set mask to look at PCINT21
+	PCMSK2 |= (1 << PCINT22); // Set mask to look at PCINT22 
 	sei();
 }
 
+void draw_path_screen() {
+	NOKIA_print(0, 0, "Show path here", 2);
+}
 
-void draw_screen() {
-	NOKIA_clear();
-	int8_t i=0;
+void draw_menu_screen() {
 	int8_t num_coords = get_num_coordinates();
+	if (is_logging) {
+		strcpy(menu[0], stop_logging_str);
+	} 
+	else {
+		strcpy(menu[0], start_logging_str);
+	}
 	char buffer[20];
 	sprintf(buffer, "Coords: %d", num_coords);
 	NOKIA_print(0,0,buffer,2);
 	NOKIA_print(0,(2+select_option)*8,">",0);
+	int8_t i=0;
 	for (i=0; i<2; i++)
 	{
 		NOKIA_print(6,8*(i+2),menu[i], 0);
+	}
+}
+
+void draw_screen() {
+	NOKIA_clear();
+	if (is_showing_path) {
+		draw_path_screen();
+	} else {
+		draw_menu_screen();
 	}
 	NOKIA_update();
 	_delay_ms(100);
@@ -79,16 +106,28 @@ void draw_screen() {
 
 void init_GUI() 
 {
-	strcpy(menu[0], "Start logging" );
+	/* Setting  variables */
+	is_logging = 0;
+	is_showing_path = 0;
+	strcpy(start_logging_str, "Start logging");
+	strcpy(stop_logging_str, "Stop logging");
+	strcpy(menu[0], start_logging_str );
 	strcpy(menu[1], "Display path" );
 	select_option = 0;
 	
+	/* Initiazing nokia display */
 	NOKIA_init(0);
 	NOKIA_LED_ENABLE();
 	NOKIA_setVop(68);
-	DDRC = 0x05;
-	DDRD &= ~(1<<PD5);
+	
+	/* Setting up input pins */
+	DDRC |= (1 << PC5);		// Set Pin C5 to output  (LED pin)
+	DDRD &= ~(1 << PD5);	// Make pin D5 input (button down)
 	PORTD |= (1 << PD5);
+	DDRD &= ~(1 << PD6);	// Make pin D6 input (button select)
+	PORTD |= (1 << PD6); 
+	
+	
 	init_pin_change_interrupt21();
 	draw_screen();
 }
