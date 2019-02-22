6bf8459611038d5d15640fa5f4e6aac5e8008b2f
Yash Seeta <yashseeta94@gmail.com>
2019-02-22 14:18:25 +0100

    Add Test case

--- MCU_GPS_LOGGER/MCU_GPS_LOGGER/main.c
+++ MCU_GPS_LOGGER/MCU_GPS_LOGGER/main.c
@@ -25,12 +25,21 @@ char GPS_DATA[BUF_SIZE];
 int main(void)
 {
 	// minimum startup with fr10esh EEPROM 
-	USART_init();
-	USART_clear_putty();
+	// USART_init();
+	// USART_clear_putty();
 
 	EEPROM_reset_header();
 
-	
+	init_GUI();
+	DDRC |= (1 << PC5);	// LED-Output
+	// gps_t GPS1,GPS2,GPS3;
+	// get_gps_coord(&GPS1, "$GPRMC,064951.000,A,2307.1256,N,12016.4438,E,0.03,165.48,260406,3.05,W,A*2C");
+	// get_gps_coord(&GPS2, "$GPRMC,064951.000,A,2307.1200,N,12016.4400,E,0.03,165.48,260406,3.05,W,A*2C");
+	// get_gps_coord(&GPS3, "$GPRMC,064951.000,A,2307.1236,N,12016.4400,E,0.03,165.48,260406,3.05,W,A*2C");
+	//  
+	// store_struct(&GPS1);
+	// store_struct(&GPS2); 
+	// store_struct(&GPS3); 
 
 	uint8_t i = 0;
 	uint16_t interval = 0;
@@ -44,18 +53,22 @@ int main(void)
 	/* Replace with your application code */
     while (1) 
     {	
+		while (!get_gps_coord(&gps,GPS_DATA)){
+			USART_receive_string(GPS_DATA, BUF_SIZE);
+		} 
 		GPS_DATA[0] = '0';
 		
 		while (!(is_gprmc(GPS_DATA))) {
 			USART_receive_string(GPS_DATA, BUF_SIZE);
 		}
+		PORTC |= (1 << PC5); // LED-Display
 		if (is_gprmc(GPS_DATA)) {
 			interval++;	
 		}
 		
 		// every GPS_INTERVALth iteration store the struct
 		if((interval % GPS_INTERVAL) == 0) {
-			USART_transmit_string("\n\rFOUND GPS DATA!\n\r");
+			// USART_transmit_string("\n\rFOUND GPS DATA!\n\r");
 			get_gps_coord(&gps, GPS_DATA);
 			print_struct(&gps);
 			store_struct(&gps);
@@ -65,13 +78,12 @@ int main(void)
 		if (i == MAX_STRUCTS) {
 			for (int j = 0; j < MAX_STRUCTS; j++) {
 				get_struct(j, &gps);
-				USART_transmit_string("\n\r at index: ");
-				USART_transmit_word(j);
-				print_struct(&gps);
+				// USART_transmit_string("\n\r at index: ");
+				// USART_transmit_word(j);
+				// print_struct(&gps);
 			}
 			while(1);
-		}
-		
+		}	
     }
 	return 0;
 }
